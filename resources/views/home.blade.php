09ff0ba2a45c177014db3312041dc9b09f09d741
serhiyosetskiy <serhiy.osetskiy@qbex.io>
2018-06-13 16:29:32 +0300

    update templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,5 @@
-<a href="{{ $gmailAuthURL }}"><img src="resources/img/google_icon64.png" /></a>
+@extends('layout.app')
+
+@section('content')
+  <a href="{{ $gmailAuthURL }}"><img src="resources/img/google_icon64.png" /></a>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Gmail-Invited</title>
+
+  <link rel="stylesheet" type="text/css" href="resources/css/gmail-invited.css">
+  <link rel="stylesheet" type="text/css" href="resources/css/loader.css">
+
+</head>
+<body>
+  <header>
+    <img src="resources/img/logo.png" />
+  </header>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
+  <script src="resources/js/jquery-1.11.1.min.js"></script>
+  <script src="resources/js/gmail-invited.js"></script>
+  
+</body>
+</html>
